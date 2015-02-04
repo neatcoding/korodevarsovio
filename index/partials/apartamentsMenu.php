969c1f61b9c6acdc950e7158b1c531a2458deef3
neatcoding <johny@toster.org>
2015-02-05 00:50:51 +0100

    linki do apartamentow

---
+++ index/partials/apartamentsMenu.php
@@ -0,0 +1,53 @@
+
+
+
+<tr>
+<td colspan="6" class="how_body">
+<table align="center" border="0">
+<tbody>
+
+
+<tr>
+<td>
+
+</td>
+
+<td> <img src="img/transparent_02.png" alt="" height="1" border="0" width="20"> </td>
+
+<td>
+
+</td>
+
+<td> <img src="img/transparent_02.png" alt="" height="1" border="0" width="20"> </td>
+
+<td>
+
+</td>
+
+<td> <img src="img/transparent_02.png" alt="" height="1" border="0" width="20"> </td>
+
+<td>
+
+</td>
+</tr>
+
+
+
+<tr><td colspan="7"><img src="img/titles.png" alt="" height="25" border="0" width="1164"></td></tr>
+
+<tr><td colspan="7"><img src="img/drop_shadow_a_top_l.png" alt="" height="10" border="0" width="22"><img src="img/drop_shadow_a.png" alt="" height="10" border="0" width="1120"><img src="img/drop_shadow_a_top_r.png" alt="" height="10" border="0" width="22"></td></tr>
+<tr>
+<td> <a href="../apartaments/solidarnosc" onmouseover="hiLite('a','a2','')" onmouseout="hiLite('a','a1','')"> <img name="a" src="img/solidarnosci.png" alt="" height="255" border="0" width="270"></a>  </td>
+<td> <img src="img/transparent_02.png" alt="" height="255" border="0" width="20"> </td>
+<td><a href="../apartaments/chmielna" onmouseover="hiLite('b','b2','')" onmouseout="hiLite('b','b1','')"> <img name="b" src="img/chmielna.png" alt="" height="255" border="0" width="270"> </a></td>
+<td> <img src="img/transparent_02.png" alt="" height="255" border="0" width="20"> </td>
+<td><a href="../apartaments/moniuszki" onmouseover="hiLite('g','g2','')" onmouseout="hiLite('g','g1','')"> <img name="g" src="img/moniuszki.png" alt="" height="255" border="0" width="270"> </a></td>
+<td> <img src="img/transparent_02.png" alt="" height="255" border="0" width="20"> </td>
+<td><a href="../apartaments/ciasna" onmouseover="hiLite('f','f2','')" onmouseout="hiLite('f','f1','')"> <img name="f" src="img/ciasna.png" alt="" height="255" border="0" width="270"> </a></td>
+</tr>
+
+<tr><td colspan="7"><img src="img/drop_shadow_bottom_l.png" alt="" height="22" border="0" width="20"><img src="img/drop_shadow.png" alt="" height="22" border="0" width="1124"><img src="img/drop_shadow_bottom_r.png" alt="" height="22" border="0" width="20"></td></tr>
+<tr><td colspan="7"><img src="img/transparent_02.png" alt="" height="48" border="0" width="30"></td></tr>
+</tbody></table>
+</td>
+</tr>
